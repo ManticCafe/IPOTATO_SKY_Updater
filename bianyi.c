1a17c10fccbb4fb127be895682319d62f7516d7f
ManticCafe <linjiatong780890@163.com>
2025-09-13 01:30:08 +0800

    缩进优化

--- bianyi.c
+++ bianyi.c
@@ -23,75 +23,66 @@
 
 bool filesize(const char *filepath) {
     struct stat file_stat;
-    
     if (stat(filepath, &file_stat) != 0) {
         return false;
     }
-    
     if (S_ISREG(file_stat.st_mode) && file_stat.st_size > MB40) {
         return true;
     }
-    
     return false;
 }
 
 bool filesize0(const char *filepath) {
     struct stat file_stat;
-    
     if (stat(filepath, &file_stat) != 0) {
         return false;
     }
-    
     if (S_ISREG(file_stat.st_mode) && file_stat.st_size > 0) {
         return true;
     }
-    
     return false;
 }
 
 int main() {
-
     system("color 75");
     SetConsoleOutputCP(CP_UTF8);
+    while ( 1 ) {
+        int num;
+        printf("土豆梦幻科技安装更新器制作助手:请先输入1编译file文件后再输入2编译安装器\n");
+        printf("输入1将file中的文件进行编译,输入2将文件编译成更新安装器,输入3退出:\n");
+        scanf("%d",&num);
+        switch (num) {
 
-while ( 1 ) {
+        case 1 :
+            const char *filename = "packed_data.c";
+            printf("更新文件正在编译中\n");
+            animate_triple_dots(3000);
+            system("python pack_folders.py");    
+            if (filesize(filename)) {
+                printf("编译完成\n");
+                break;
+            }
+        break;
 
-    int num;
-    printf("土豆梦幻科技安装更新器制作助手:请先输入1编译file文件后再输入2编译安装器\n");
-    printf("输入1将file中的文件进行编译,输入2将文件编译成更新安装器,输入3退出:\n");
-    scanf("%d",&num);
-    switch (num)
-    {
-    case 1 :
-        const char *filename = "packed_data.c";
-        printf("更新文件正在编译中\n");
-        animate_triple_dots(3000);
-        system("python pack_folders.py");    
-        if (filesize(filename)) {
-            printf("编译完成\n");
-            break;
+        case 2 :
+            const char *filename1 = "folder_extractor.exe";
+            printf("正在编译程序\n");
+            animate_ellipsis(3000);
+            system("gcc -o folder_extractor main.c packed_data.c zlib/*.c -Izlib");
+            if (filesize0(filename1)) {
+                printf("编译成功\n");
+                break;
+            }
+        break;
+
+        case 3 :
+            return 0;
+        break;
+
+        default :
+            printf("输入有误\n");
+
         }
-        break;
-
-    case 2 :
-        const char *filename1 = "folder_extractor.exe";
-        printf("正在编译程序\n");
-        animate_ellipsis(3000);
-        system("gcc -o folder_extractor main.c packed_data.c zlib/*.c -Izlib");
-        if (filesize0(filename1)) {
-            printf("编译成功\n");
-            break;
-        }
-        break;
-
-    case 3 :
-        return 0;
-        break;
-
-    default :
-        printf("输入有误\n");
     }
-}
-
     return 0;
 }
